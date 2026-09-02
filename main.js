147ef12f145f4bb504961913d96adc4726510c26
Yikxzyz <angelrubiotrabajos@gmail.com>
2026-09-02 08:20:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@
     { date: "2026-08-23", city: "Toronto", country: "Canadá", venue: "Rogers Stadium", region: "north-america", surpriseSongs: ["00:00 (Zero O'clock)", "Outro: Tear"] },
     { date: "2026-08-27", city: "Chicago", country: "Estados Unidos", venue: "Soldier Field", region: "north-america", surpriseSongs: ["Tomorrow", "Hip Hop Phile"] },
     { date: "2026-08-28", city: "Chicago", country: "Estados Unidos", venue: "Soldier Field", region: "north-america", surpriseSongs: ["134340", "Mikrokosmos"] },
-    { date: "2026-09-01", city: "Los Ángeles", country: "Estados Unidos", venue: "SoFi Stadium", region: "north-america", surpriseSongs: null },
+    { date: "2026-09-01", city: "Los Ángeles", country: "Estados Unidos", venue: "SoFi Stadium", region: "north-america", surpriseSongs: ["Boy in Luv", "Magic Shop"] },
     { date: "2026-09-02", city: "Los Ángeles", country: "Estados Unidos", venue: "SoFi Stadium", region: "north-america", surpriseSongs: null },
     { date: "2026-09-05", city: "Los Ángeles", country: "Estados Unidos", venue: "SoFi Stadium", region: "north-america", surpriseSongs: null },
     { date: "2026-09-06", city: "Los Ángeles", country: "Estados Unidos", venue: "SoFi Stadium", region: "north-america", surpriseSongs: null },
